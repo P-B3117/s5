9ace8b76299c5341215251dd3f69d7b2263e0f39
Laurent Stéphenne <laurent@guibi.dev>
2026-01-07 12:01:09 -0500

    Lab part2 clean

--- lab/exercice2.m
+++ lab/exercice2.m
@@ -13,33 +13,46 @@ accelLinLame = (OB * omegaOB^2 * sin(teta)) + (AB * omegaAB.^2 .* cos(beta)) + (
 figure;
 
 % Blade speed subplot
-subplot(2, 2, 1);
+subplot(3, 2, 1);
 plot(teta, velLame, 'LineWidth', 2);
 grid on;
 xlabel('Angle teta (deg)');
 ylabel('Velocity (m/s)');
-title('Acceleration Profile of Lame vs Angle');
+title('Acceleration Profile of Lame vs Rotation');
+xlim([0, 2*pi]);
 
-% Angular blade speed subplot
-subplot(2, 2, 2);
+% Angular AB speed subplot
+subplot(3, 2, 3);
 plot(teta, omegaAB, 'LineWidth', 2);
 grid on;
 xlabel('Angle teta (deg)');
 ylabel('Angular Velocity (rad/s)');
-title('Angular Velocity Profile of AB vs Angle');
+title('Angular Velocity Profile of AB vs Rotation');
+xlim([0, 2*pi]);
 
-% Linear blade speed subplot
-subplot(2, 2, 3);
+% Linear blade acceleration subplot
+subplot(3, 2, 2);
 plot(teta, accelLinLame, 'LineWidth', 2);
 grid on;
 xlabel('Angle teta (deg)');
 ylabel('Acceleration (m/s^2)');
-title('Acceleration Profile of Lame vs Angle');
+title('Acceleration Profile of Lame vs Rotation');
+xlim([0, 2*pi]);
 
-% Angular blade speed subplot
-subplot(2, 2, 4);
+% Angular AB acceleration subplot
+subplot(3, 2, 4);
 plot(teta, alphaAB, 'LineWidth', 2);
 grid on;
 xlabel('Angle teta (deg)');
 ylabel('Angular Acceleration (rad/s^2)');
-title('Angular Acceleration Profile of AB vs Angle');
+title('Angular Acceleration Profile of AB vs Rotation');
+xlim([0, 2*pi]);
+
+% AB angle subplot
+subplot(3, 2, 5);
+plot(teta, beta, 'LineWidth', 2);
+grid on;
+xlabel('Angle teta (deg)');
+ylabel('Angle beta (rad)');
+title('Angle Profile of AB vs Rotation');
+xlim([0, 2*pi]);
