c4c15317cf4b7fc94c2af4e13cfedf5072cac31b
PB-3117 <zacse38@gmail.com>
2026-02-09 13:29:50 -0500

    rectified wav loader

--- app3/wavLoader.py
+++ app3/wavLoader.py
@@ -12,4 +12,16 @@ def load_guitare() -> tuple[int, np.ndarray]:
 
 
 def load_basson() -> tuple[int, np.ndarray]:
-    return load_wav("./sounds/note_basson_plus_sinus_1000hz.wav")
+    return load_wav("./sounds/note_basson_plus_sinus_1000_hz.wav")
+
+
+if __name__ == "__main__":
+    print("Running wavLoader functions:")
+    print()
+    sample_rate, data = load_guitare()
+    print(f" - Guitar Sample rate: {sample_rate} Hz")
+    print(f" - Guitar Data shape: {data.shape}")
+    print()
+    sample_rate, data = load_basson()
+    print(f" - Bassoon Sample rate: {sample_rate} Hz")
+    print(f" - Bassoon Data shape: {data.shape}")
