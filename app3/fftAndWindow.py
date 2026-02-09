4a3e4feed5895125ebcb87bd9a8d8f52145a1058
PB-3117 <zacse38@gmail.com>
2026-02-09 15:39:53 -0500

    fft works?

--- app3/fftAndWindow.py
+++ app3/fftAndWindow.py
@@ -1,46 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.fft import fft, fftshift
+from numpy.fft import rfft
 from scipy.signal.windows import hamming
 
 NORMAL_SPAN = 2 * np.pi
 
 
-def apply_fft(sig: np.ndarray, span=NORMAL_SPAN) -> tuple[np.ndarray, np.ndarray]:
-    fft_filter = fftshift(fft(sig) * hamming(len(sig)))
+def apply_window(sig: np.ndarray, window_func=hamming) -> np.ndarray:
+    return sig * window_func(len(sig))
+
+
+def apply_fft(sampling_rate: float, sig: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    fft_filter = rfft(sig)
+    N = len(sig)
     # fft_filter = fftshift(fft(sig) * np.hanning(len(sig)))
 
-    xpoints = np.linspace(-span / 2, span / 2, len(fft_filter))
+    xpoints = np.fft.rfftfreq(N, d=1 / sampling_rate)
 
     return xpoints, fft_filter
 
 
 def apply_fft_magnitude(
+    sampling_rate: float | None = None,
     sig: np.ndarray | None = None,
     xpoints: np.ndarray | None = None,
     fft_filter: np.ndarray | None = None,
 ) -> tuple[np.ndarray, np.ndarray]:
 
-    if sig is None:
+    if sig is None or sampling_rate is None:
         if xpoints is not None and fft_filter is not None:
             return xpoints, np.abs(fft_filter)
         else:
-            raise ValueError("Either sig or xpoints and fft_filter must be provided")
+            raise ValueError(
+                "Either sig and sampling rates or xpoints and fft_filter must be provided"
+            )
 
-    xpoints, fft_filter = apply_fft(sig)
+    xpoints, fft_filter = apply_fft(sampling_rate, sig)
 
     return xpoints, np.abs(fft_filter)
 
 
-def plot_fft(
-    xpoints: np.ndarray, fft_filter: np.ndarray, span=NORMAL_SPAN, title="FFT"
-):
+def plot_fft(xpoints: np.ndarray, fft_filter: np.ndarray, title="FFT"):
+    print(len(xpoints))
+    print(len(fft_filter))
     plt.figure()
     plt.stem(xpoints, np.abs(fft_filter))
     plt.title(title)
     plt.xlabel("Frequency")
     plt.ylabel("Magnitude")
-    plt.xlim(-span / 2, span / 2)
     plt.grid(True)  # good ol grid on
 
 
@@ -52,8 +59,7 @@ def test():
     t = np.linspace(0, N / fs, N)
 
     # 3. Apply Windowing (Hann)
-    window = hamming(N)
-    windowed_signal = signal * window
+    windowed_signal = apply_window(signal)
 
     # 4. Perform Real FFT
     fft_out = np.fft.rfft(windowed_signal)
@@ -86,27 +92,29 @@ def test():
 
 
 if __name__ == "__main__":
-    # from wavLoader import load_basson, load_guitare
+    from wavLoader import load_basson, load_guitare
 
-    # print("Plotting fft and magnitude of sounds using a Hamming window:")
-    # print()
-    # sample_rate, data = load_guitare()
-    # print(" - plotting for Guitar")
-    # xpoints, fft_filter = apply_fft(data)
-    # plot_fft(xpoints, fft_filter, title="FFT Guitar")
-    # plot_fft(
-    #     *apply_fft_magnitude(xpoints=xpoints, fft_filter=fft_filter),
-    #     title="Magnitude Guitar",
-    # )
-    # print()
-    # print(" - plotting for Bassoon")
-    # sample_rate, data = load_basson()
-    # xpoints, fft_filter = apply_fft(data)
-    # plot_fft(xpoints, fft_filter, title="FFT Bassoon")
-    # plot_fft(
-    #     *apply_fft_magnitude(xpoints=xpoints, fft_filter=fft_filter),
-    #     title="Magnitude Bassoon",
-    # )
+    print("Plotting fft and magnitude of sounds using a Hamming window:")
+    print()
+    sample_rate, data = load_guitare()
+    print(" - plotting Guitare fft")
+    xpoints, fft_filter = apply_fft(sample_rate, data)
+    plot_fft(xpoints, fft_filter, title="FFT Guitar")
+    print(" - plotting Guitare magnitude")
+    plot_fft(
+        *apply_fft_magnitude(xpoints=xpoints, fft_filter=fft_filter),
+        title="Magnitude Guitar",
+    )
+    print()
+    print(" - plotting Bassoon fft")
+    sample_rate, data = load_basson()
+    xpoints, fft_filter = apply_fft(sample_rate, data)
+    plot_fft(xpoints, fft_filter, title="FFT Bassoon")
+    print(" - plotting Bassoon magnitude")
+    plot_fft(
+        *apply_fft_magnitude(xpoints=xpoints, fft_filter=fft_filter),
+        title="Magnitude Bassoon",
+    )
     #
-    test()
+    # test()
     plt.show()
